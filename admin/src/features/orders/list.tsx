a08e8205e08e491acb7e328fd714187e99e977c4
sobogd <sobogd@gmail.com>
2023-01-12 14:30:42 +0300

    fix ordring and add summary

--- admin/src/features/orders/list.tsx
+++ admin/src/features/orders/list.tsx
@@ -8,6 +8,13 @@ import { ordersSlice } from "../../slices/orders";
 import { tablesService } from "../../services/tables";
 import Loading from "../../shared/loading";
 
+const roundFive = (a: any) => {
+  var b = a % 5;
+  b && (a = a - b + 5);
+
+  return a;
+};
+
 export const OrdersList: React.FC = () => {
   const dispatch = useAppDispatch();
   const { items, isLoading } = useAppSelector((s) => s.orders);
@@ -30,13 +37,14 @@ export const OrdersList: React.FC = () => {
       <List disablePadding>
         {items?.map((i) => {
           const table = tableItems.find((t) => t.id === i.tableId);
-          const positionsIds = i.ordersPositions?.map((op) => Number(op.positionId)) || [];
+
           const additionalsForPosition = i.ordersPositions
             ?.filter((op) => !!op.additional?.length)
             .map((op) => op.additional);
-          const positionsList: any = positionsItems
-            .filter((pi) => positionsIds.includes(Number(pi.id)))
-            .map((pi) => `${pi.name} - ${pi.price}`);
+          const positionsList: any = i.ordersPositions?.map((op) => {
+            const position = positionsItems.find((pi) => Number(pi.id) === Number(op.positionId));
+            return { ...position, additional: op.additional };
+          });
 
           const arrayAdditional: any = [];
           if (!!additionalsForPosition?.length) {
@@ -64,22 +72,60 @@ export const OrdersList: React.FC = () => {
                 primary={`${table?.number} - ${table?.name}`}
                 secondary={
                   <>
-                    {positionsList.map((e: any) => {
-                      return (
-                        <div>
-                          {e}
-                          <br></br>
+                    {(() => {
+                      let summary = 0;
+
+                      const r = [];
+
+                      r.push(
+                        positionsList.map((e: any) => {
+                          const result = [];
+
+                          result.push(
+                            <div>
+                              {e?.name} {!!Number(e?.price) && <b style={{ fontWeight: 600 }}>{e?.price}</b>}
+                            </div>
+                          );
+
+                          summary = summary + Number(e.price);
+
+                          if (e.additional?.length) {
+                            const splitted = e.additional?.split("/");
+                            splitted?.forEach((spl: any) => {
+                              console.log({ spl });
+                              const colAndId = spl.split("-");
+                              const pos = positionsItems.find((pi) => Number(pi.id) === Number(colAndId[1]));
+                              const price = Number(pos?.price) * Number(colAndId[0]);
+                              summary = summary + price;
+
+                              result.push(
+                                <div style={{ paddingLeft: 20 }}>
+                                  {pos?.name}{" "}
+                                  {!!Number(pos?.price) && <b style={{ fontWeight: 600 }}>{pos?.price}</b>}
+                                  <br></br>
+                                </div>
+                              );
+                            });
+                          }
+
+                          return result;
+                        })
+                      );
+
+                      r.push(
+                        <div style={{ marginTop: 5, fontWeight: 400, color: "white" }}>
+                          Итого картой: {summary}
                         </div>
                       );
-                    })}
-                    {arrayAdditional.map((e: any) => {
-                      return (
-                        <div style={{ paddingLeft: 20 }}>
-                          {e}
-                          <br></br>
+
+                      r.push(
+                        <div style={{ marginTop: 5, fontWeight: 400, color: "white" }}>
+                          Итого наличными: {roundFive(summary * 0.9)}
                         </div>
                       );
-                    })}
+
+                      return r;
+                    })()}
                   </>
                 }
               />
